70c6a9abb4e6361bae236863e81c832a92107094
grahaminn <grahami@blinkbox.com>
2014-11-15 01:40:14 +0000

    exercise 11

---
+++ ex11/ex11.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // go through each string in argv
+
+    int i = argc;
+    while(i > 0) {
+        i--;
+        printf("arg %d: %s\n", i, argv[i]);
+    }
+
+    // let's make our own array of strings
+    char *states[] = {
+        "California", "Oregon",
+        "Washington", "Texas"
+    };
+  
+    i = 0;
+    while (i < 4 && i < argc) {
+      states[i] = argv[i];
+      i++;
+    }
+
+    int num_states = 4;
+    i = 0;  // watch for this
+    while(i < num_states) {
+        printf("state %d: %s\n", i, states[i]);
+        i++;
+    }
+
+    return 0;
+}
